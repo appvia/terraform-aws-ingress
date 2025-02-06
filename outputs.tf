a96945409956fcdbb670ed0f7f3d28427cfaf0f6
danny-molnar <danny.molnar@appvia.io>
2025-02-06 15:57:00 +0000

    chore: update outputs

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,27 @@ output "alb_dns_name" {
   value       = aws_lb.lb.dns_name
 }
 
+output "alb_zone_id" {
+  description = "The Zone ID of the ALB/NLB (useful for Route 53 alias records)"
+  value       = aws_lb.lb.zone_id
+}
+
 output "target_group_arns" {
   description = "A map of Target Group ARNs"
   value       = { for tg in aws_lb_target_group.backend : tg.name => tg.arn }
 }
 
-output "waf_web_acl_association" {
+output "target_group_names" {
+  description = "A map of Target Group names"
+  value       = { for tg in aws_lb_target_group.backend : tg.name => tg.name }
+}
+
+output "waf_web_acl_arn" {
   description = "The associated WAF Web ACL ARN (if applied)"
-  value       = length(aws_wafv2_web_acl_association.lb_waf_assoc) > 0 ? aws_wafv2_web_acl_association.lb_waf_assoc[0].web_acl_arn : null
+  value       = var.waf_rule_group_name != "" ? aws_wafv2_web_acl_association.alb_waf_assoc[0].web_acl_arn : null
+}
+
+output "security_group_id" {
+  description = "The security group ID assigned to the ALB/NLB"
+  value       = aws_security_group.baseline_sg.id
 }
